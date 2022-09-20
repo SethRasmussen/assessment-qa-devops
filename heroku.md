e08d7e1ec5521d911be12e08717ffe27f9ce4d75
Seth Rasmussen <sethrsmssn792@gmail.com>
2022-09-19 22:05:08 -0600

    added heroku.md

--- heroku.md
+++ heroku.md
@@ -1,2 +1,2 @@
 ## Paste your link below
-
+https://assement-qa-devops-1.herokuapp.com/
--- server.js
+++ server.js
@@ -16,11 +16,6 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-
-
-
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
